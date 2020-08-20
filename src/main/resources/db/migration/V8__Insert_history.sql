fb2561d251cc7fd00f0ff77d5dcc49ae4a52b3b1
konradbanburski <36411796+konradbanburski@users.noreply.github.com>
2020-08-20 23:21:07 +0200

    --

---
+++ src/main/resources/db/migration/V8__Insert_history.sql
@@ -0,0 +1,5 @@
+insert into HISTORY (ID_ACCOUNT, ID_JOBS, ID_CLIENT, DATA, TIME) values ('2', '1','1','2020-08-18','00:25:00');
+insert into HISTORY (ID_ACCOUNT, ID_JOBS, ID_CLIENT, DATA, TIME) values ('2', '4','1','2020-08-18','00:20:00');
+insert into HISTORY (ID_ACCOUNT, ID_JOBS, ID_CLIENT, DATA, TIME) values ('2', '2','1','2020-08-18','00:30:00');
+
+
